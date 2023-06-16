22fc0562fc18a513413700835b1843a8836aaa5e
Khoo Jing Xiang <122257205+meowwtama@users.noreply.github.com>
2023-06-16 20:19:50 +0800

    Add Grapple Mechanic
    Added a new Grapple Layer that players can grapple towards.
    Players will drop in an arc upon releasing grapple.
    Also included a tongue-like animation to serve as the grapple "rope".

---
+++ FrogChamp/Assets/Scripts/GrapplingTongue.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class GrapplingTongue : MonoBehaviour
+{
+    [Header("General References:")]
+    public Movement movement;
+    public LineRenderer m_lineRenderer;
+
+    [SerializeField] private int precision = 40;
+    //float moveTime = 0;
+    public bool isGrappling = true;
+
+    private void OnEnable()
+    {
+        m_lineRenderer.positionCount = precision;
+        LinePointsToFirePoint();
+        m_lineRenderer.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        m_lineRenderer.enabled = false;
+        isGrappling = false;
+    }
+
+    private void Update()
+    {
+        DrawTongue();
+    }
+
+    private void LinePointsToFirePoint()
+    {
+        for (int i = 0; i < precision; i++)
+        {
+            m_lineRenderer.SetPosition(i, movement.firePoint.position);
+        }
+    }
+
+    void DrawTongue()
+    {
+        if (!isGrappling)
+        {
+            movement.Grapple();
+            isGrappling = true;
+        }
+        if (m_lineRenderer.positionCount != 2) { m_lineRenderer.positionCount = 2; }
+        m_lineRenderer.SetPosition(0, movement.firePoint.position);
+        m_lineRenderer.SetPosition(1, movement.grapplePoint);
+    }
+}
--- FrogChamp/Assets/Scripts/Movement.cs
+++ FrogChamp/Assets/Scripts/Movement.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    // variables for grapple mechanics
+
+    [Header("Grapple references:")]
+    public GrapplingTongue grappleTongue;
+    [SerializeField] private int grappableLayerNumber = 8;
+    public Camera m_camera;
+    public Transform tongueHolder;
+    public Transform firePoint;
+    public SpringJoint2D m_springJoint2D;
+    [SerializeField] private float maxDistance = 10;
+
+    public Vector2 grapplePoint;
+    public Vector2 grappleDistanceVector;
+
+    [Header("Jump references:")]
     [SerializeField] private int speed;
     [SerializeField] private int jumpSpeed;
     [SerializeField] private float maxHoldDuration;
@@ -16,6 +31,12 @@ public class Movement : MonoBehaviour
     private Rigidbody2D body;
     // private BoxCollider2D boxCollider;
     private CapsuleCollider2D capsuleCollider;
+
+    private void Start()
+    {
+        grappleTongue.enabled = false;
+        m_springJoint2D.enabled = false;
+    }
     
 
     // Awake is called every time the script is loaded
@@ -68,6 +89,84 @@ public class Movement : MonoBehaviour
         /*if (ContactWall() && !IsGrounded())
             Bounce();*/
 
+        // Grapple logic
+
+        // When player chooses a grapple point. Checks if grapple point is valid and starts the grapple process.
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            SetGrapplePoint();
+        }
+        
+        // When player is holding down left mouse button (grappling).
+        else if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (!grappleTongue.enabled)
+            {
+                Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+            }
+
+            if (grappleTongue.isGrappling)
+            {
+                body.velocity = Vector2.zero;
+                Vector2 firePointDistance = firePoint.position - tongueHolder.localPosition;
+                Vector2 targetPos = grapplePoint - firePointDistance;
+                tongueHolder.position = Vector2.Lerp(tongueHolder.position, targetPos, Time.deltaTime * 3);
+            }
+        }
+
+        // When player releases the left mouse button (releases grapple).
+        else if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            if (grappleTongue.enabled)
+            {
+                body.velocity = new Vector2(GetPlayerDirection() * 10.0f, body.velocity.y);
+                grappleTongue.enabled = false;
+                m_springJoint2D.enabled = false;
+                body.gravityScale = 2.5F;
+            }
+        }
+
+        // When left mouse button is not held down.
+        else
+        {
+            Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+    }
+
+    // Checks if grapple point selected is valid and is in radius range.
+    // Player cannot grapple when not grounded.
+    void SetGrapplePoint()
+    {
+        Vector2 distanceVector = m_camera.ScreenToWorldPoint(Input.mousePosition) - firePoint.position;
+        if (Physics2D.Raycast(firePoint.position, distanceVector.normalized))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(firePoint.position, distanceVector.normalized, distanceVector.magnitude);
+            if (hit.transform.gameObject.layer == grappableLayerNumber && IsGrounded())
+            {
+                if (Vector2.Distance(hit.point, firePoint.position) <= maxDistance)
+                {
+                    grapplePoint = hit.point;
+                    grappleDistanceVector = grapplePoint - (Vector2)firePoint.position;
+                    grappleTongue.enabled = true;
+                }
+            }
+        }
+    }
+
+    public void Grapple()
+    {
+        body.gravityScale = 0;
+        body.velocity = Vector2.zero;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (firePoint != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(firePoint.position, maxDistance);
+        }
     }
 
     // Jump in a fixed arc based on your directional input
@@ -81,12 +180,16 @@ public class Movement : MonoBehaviour
 
         // if left or right is pressed, jump left or right
         // if no directional input is pressed, jump upwards
-        if (horizontalInput > 0.01f) 
-            body.velocity = new Vector2(speed, holdFactor * jumpSpeed);
-        else if (horizontalInput < -0.01f)
-            body.velocity = new Vector2(-speed, holdFactor * jumpSpeed);
-        else if(horizontalInput == 0.0f)
-            body.velocity = new Vector2(0, holdFactor * jumpSpeed);
+        body.velocity = new Vector2(GetPlayerDirection() * speed, holdFactor * jumpSpeed);
+    }
+
+    // Returns 1 if player is facing right, -1 if player is facing left, else returns 0.
+    private int GetPlayerDirection()
+    {
+        if (horizontalInput > 0.01f) { return 1; }
+        else if (horizontalInput < -0.01f) { return -1; }
+        else { return 0; }
+
     }
 
     // Bounce away from platform when player contacts platform from the sides
@@ -95,7 +198,7 @@ public class Movement : MonoBehaviour
         body.velocity = new Vector2(-body.velocity.x, body.velocity.y);
     }
 
-    // Check if player is on the platorm
+    // Check if player is on the platform
     private bool IsGrounded()
     {
         Vector3 boxSize = capsuleCollider.bounds.size;
@@ -107,7 +210,7 @@ public class Movement : MonoBehaviour
         return raycastHit.collider != null;*/
     }
 
-    // Check if player contacts platform from the sides
+    // Check if player contacts platform from the sides (NOT IN USE)
     private bool ContactWall()
     {
         Vector3 boxSize = capsuleCollider.bounds.size;
