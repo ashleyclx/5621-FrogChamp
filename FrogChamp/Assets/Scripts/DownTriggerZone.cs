203b95b4f33a73e4db2ee7d3564258c8c1085e8b
Khoo Jing Xiang <122257205+meowwtama@users.noreply.github.com>
2023-06-21 16:45:30 +0800

    Fixed camera script
    Initially if player moved too fast through trigger zone, camera might not be able to keep up with screen change.
    Fixed issue by adding 2 separate trigger zones for up and down screen changes.

--- FrogChamp/Assets/Scripts/DownTriggerZone.cs
+++ FrogChamp/Assets/Scripts/DownTriggerZone.cs
@@ -2,19 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerZone : MonoBehaviour
+public class DownTriggerZone : MonoBehaviour
 {
     [SerializeField] private Transform prevScreen;
-    [SerializeField] private Transform nextScreen;
     [SerializeField] private CameraMovement cam;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            if (collision.transform.position.y < transform.position.y)
-                cam.ChangeScreen(nextScreen);
-            else
+            if (collision.transform.position.y > transform.position.y)
                 cam.ChangeScreen(prevScreen);
         }
     }
