bcc8e1e6ae6ce463293a4d176771026e16777e43
ashleyclx <e0958221@u.nus.edu>
2023-07-03 16:15:30 +0800

    Made Firebase Manager & login
    Also started on Login screens for UI manager
    Deleted Firebase > Plugins > x86-64 > FirebaseCppApp-11_2_0.bundle because file size too big. Note: should be fine because not using c++

--- FrogChamp/Assets/Scripts/UI/UIManager.cs
+++ FrogChamp/Assets/Scripts/UI/UIManager.cs
@@ -6,11 +6,29 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public static UIManager instance;
+
     [Header("Pause Menu")]
     [SerializeField] GameObject pauseScreen;
 
-    private void Start()
+    [Header("Firebase")]
+    public GameObject loginUI;
+    public GameObject registerUI;
+    public GameObject userDataUI;
+    public GameObject leaderboardUI;
+
+    private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+
         pauseScreen.SetActive(false);
     }
 
@@ -80,6 +98,39 @@ public class UIManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         PauseGame(false);
+    }
+    #endregion
+
+    #region Firebase
+    public void ClearScreen() //Turn off all screens
+    {
+        loginUI.SetActive(false);
+        registerUI.SetActive(false);
+        userDataUI.SetActive(false);
+        leaderboardUI.SetActive(false);
+    }
+
+    public void LoginScreen() //Back button
+    {
+        ClearScreen();
+        loginUI.SetActive(true);
+    }
+    public void RegisterScreen() // Regester button
+    {
+        ClearScreen();
+        registerUI.SetActive(true);
+    }
+
+    public void UserDataScreen() //Logged in
+    {
+        ClearScreen();
+        userDataUI.SetActive(true);
+    }
+
+    public void LeaderboardScreen() //Scoreboard button
+    {
+        ClearScreen();
+        leaderboardUI.SetActive(true);
     }
     #endregion
 }
